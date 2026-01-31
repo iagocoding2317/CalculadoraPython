5ef2fe0f8ade256d49e85d87ffebbf38d5bda556
iagocoding2317 <115094044+iagocoding2317@users.noreply.github.com>
2026-01-31 11:46:54 -0300

    Implement basic calculator functionality

---
+++ Calculadora.py
@@ -0,0 +1,21 @@
+num1= int(input("Digite seu primeiro número: "))
+
+num2 = int(input("Digite seu segundo número: "))
+
+print("Escolha sua opção: 1-Soma, 2-Subtração, 3-Multiplicação, 4-Divisão")
+opcao = int(input("Digite sua opcão: "))
+
+
+
+if opcao == 1:
+  print(num1 + num2)
+elif opcao == 2:
+  print(num1 + num2)
+elif opcao == 3:
+  print(num1 * num2)
+elif opcao == 4 and num2 != 0:
+  print(num1/num2)
+elif opcao == 4 and num2 == 0:
+  print("Erro")
+else:
+  print("Erro")
