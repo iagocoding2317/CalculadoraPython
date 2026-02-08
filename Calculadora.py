a91d9a42680569a13d89fd0aee8e65fe3340a5d9
iagocoding2317 <115094044+iagocoding2317@users.noreply.github.com>
2026-02-07 23:59:58 -0300

    Change input type to float and improve error messages

--- Calculadora.py
+++ Calculadora.py
@@ -1,6 +1,6 @@
-num1= int(input("Digite seu primeiro número: "))
+num1= float(input("Digite seu primeiro número: "))
 
-num2 = int(input("Digite seu segundo número: "))
+num2 = float(input("Digite seu segundo número: "))
 
 print("Escolha sua opção: 1-Soma, 2-Subtração, 3-Multiplicação, 4-Divisão")
 opcao = int(input("Digite sua opcão: "))
@@ -16,6 +16,8 @@ elif opcao == 3:
 elif opcao == 4 and num2 != 0:
   print(num1/num2)
 elif opcao == 4 and num2 == 0:
-  print("Erro")
+  print("Erro. Não dá pra dividir por zero")
 else:
-  print("Erro")
+  print(f"{opcao} não é válido")
+
+
